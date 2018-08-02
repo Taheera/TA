883876d1bf68fa02aff19b3cd6c6ad48f2cd44fd
Taheera <42031282+Taheera@users.noreply.github.com>
2018-08-02 21:57:48 +0530

    Update api_5.py

--- api_5.py
+++ api_5.py
@@ -47,11 +47,11 @@ def processRequest(req):
     ques1 = ques.replace(" ","+")
     yql_url = baseurl + str(ques1)
     result = urlopen(yql_url).read()
-    data = json.loads(result)
+    data1 = json.loads(result)
     #for some the line above gives an error and hence decoding to utf-8 might help
     #data = json.loads(result.decode('utf-8'))
     #res = makeWebhookResult(data)
-    speech = str(data)
+    speech = str(data1)
     print("Response:")
     print(speech)
     return {
