04fdf02a8e8915ac80f0f6c4b70004d7da118b54
Taheera <42031282+Taheera@users.noreply.github.com>
2018-08-03 11:54:11 +0530

    Update api_6.py

--- api_6.py
+++ api_6.py
@@ -37,7 +37,7 @@ def webhook():
 def processRequest(req):
     if req.get("queryResult").get("action") != "interest":
         return {}
-    baseurl = "http://c5965143.ngrok.io/api/v1/resources/books?questions="
+    baseurl = "http://ec5c561f.ngrok.io/api/v1/resources/books?questions="
     #yql_query = makeYqlQuery(req)
     #if yql_query is None:
         #return {}
