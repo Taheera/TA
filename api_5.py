46da44a9a77b752960071bc36cb03772064d2fe9
Taheera <42031282+Taheera@users.noreply.github.com>
2018-08-06 16:04:25 +0530

    Update api_5.py

--- api_5.py
+++ api_5.py
@@ -48,7 +48,7 @@ def processRequest(req):
     yql_url = baseurl + str(ques1)
     result = urlopen(yql_url).read()
     data1 = json.loads(result)
-    if data1 == 0:
+    if data1[0] == 0:
         #print("Response:")
         speech = "please try another question."
         return {
