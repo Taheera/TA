645bb962dcced895329dfdeaa1371eac6fbc0d77
Taheera <42031282+Taheera@users.noreply.github.com>
2018-08-09 17:59:52 +0530

    Update webhookApi.py

--- webhookApi.py
+++ webhookApi.py
@@ -63,7 +63,7 @@ def processRequest(req):
     #if req.get("queryResult").get("action") != "interest":
     if req.get("queryResult").get("action") != "osteo-info":
         return {}
-    baseurl = "http://9b8e1d2d.ngrok.iohttp://59d20dae.ngrok.io/api/v1/resources/books?questions="
+    baseurl = "http://9b8e1d2d.ngrok.io/api/v1/resources/books?questions="
     #yql_query = makeYqlQuery(req)
     #if yql_query is None:
         #return {}
