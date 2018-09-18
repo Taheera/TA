2516df35d3e080f61c1ed759d96ff6f06ca4ec9e
Taheera <42031282+Taheera@users.noreply.github.com>
2018-09-18 10:16:54 +0000

    Update api_5.py

--- api_5.py
+++ api_5.py
@@ -38,7 +38,7 @@ def processRequest(req):
     #if req.get("queryResult").get("action") != "interest":
     if req.get("queryResult").get("action") != "osteo-info":
         return {}
-    baseurl = "https://28f43fcf.ngrok.io/api/v1/resources/books?questions="
+    baseurl = "https://be87ef6a.ngrok.io/api/v1/resources/books?questions="
     #yql_query = makeYqlQuery(req)
     #if yql_query is None:
         #return {}
