2f4f52d7490549f31d5b02ac52ee8b33c207b09e
Taheera <42031282+Taheera@users.noreply.github.com>
2018-09-19 19:01:45 +0530

    Update Test_1.py

--- Test_1.py
+++ Test_1.py
@@ -22,5 +22,13 @@ def addjd():
         return "success" + " " + b
     else:
         return "false"
-app.run(debug=True, port=port, host='0.0.0.0')
+    
+if __name__ == '__main__':
+
+
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
 
