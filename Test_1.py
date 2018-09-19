7807a31400af5acc53d94bf75eb3b5b5f5a25da0
Taheera <42031282+Taheera@users.noreply.github.com>
2018-09-19 17:13:32 +0530

    Update Test_1.py

--- Test_1.py
+++ Test_1.py
@@ -1,39 +1,18 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
 from __future__ import print_function
 import flask
 from flask import request, jsonify
 import re, math
-
-
-# In[2]:
-
-
 import json
 import os
 
 
-# In[3]:
-
-
 app = flask.Flask(__name__)
-app.config["DEBUG"] = False
-
-
-# In[4]:
-
+app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>API</h1><p>Test Api</p>"
-
-
-# In[ ]:
-
 
 @app.route('/api', methods=['GET'])
 def addjd():
