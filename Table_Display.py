091df4b8f37f7c338f133983e977cc2bb676941f
Taheera <42031282+Taheera@users.noreply.github.com>
2018-10-08 15:25:51 +0530

    Update Table_Display.py

--- Table_Display.py
+++ Table_Display.py
@@ -24,5 +24,12 @@ def home():
     except Exception as e:
         return (str(e))
     
-app.run()
+if __name__ == '__main__':
+
+
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
 
